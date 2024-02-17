6e4dc58bd70126dcfa6c1868b8595206e297fd56
Mikhail Dronov <mike@dronov.net>
2024-02-17 16:06:24 +0100

    add custom ssh_key, resize root partition to 50gb by default

--- create-ubuntu-jammy-template.sh
+++ create-ubuntu-jammy-template.sh
@@ -9,6 +9,7 @@ tmp_cores="2"
 tmp_memory="2048"
 rootPasswd="password"
 cpuTypeRequired="host"
+sshKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM92Vb893P/I2bBiJeY0Q7kNjfizy4nVcj1lRS08VyVHpLCivBIZLFn0eKDss2LbSh0oXnrFug4Ez3vnRi63Sg1fPgUtPP0omwoQahlFTLmZ27mYeDkRxD3mCrjeFwajaqjwnhV3dvAe9+1D4Na9J0cq2RZphKzPLaNRM74qrHGb1RIsUqPFbWlDRKenOP2kvS/lEIVqKpnlFnqK3NPh4+27D0od5+doYx6Q9WFwPpVSNyZf9UXsknZhvuS6P2xVcfm2pTZBeAlhbHILdw/3HVs+koA8YmedH9bWy/ZEKl6gGkpErAm/lfEbkmj2H0ZmgkAJaIvODJskH5Fzal0HMp mishadronov"
 
 apt update
 apt install libguestfs-tools -y
@@ -16,9 +17,17 @@ rm *.img
 wget -O $imageName $imageURL
 qm destroy $virtualMachineId
 virt-customize -a $imageName --install qemu-guest-agent
-virt-customize -a $imageName --root-password password:$rootPasswd
+virt-customize -a $imageName --root-password password:$rootPasswd --ssh-inject root:file:/root/.ssh/authorized_keys
+echo $sshKey > authorized_keys
+virt-customize -a $imageName --upload authorized_keys:/root/.ssh/authorized_keys
+
+qemu-img resize $imageName +50G
+mv $imageName ${imageName/.img/-resized.img}
+
+virt-resize --expand /dev/sda1 ${imageName/.img/-resized.img} $imageName
+
 qm create $virtualMachineId --name $templateName --memory $tmp_memory --cores $tmp_cores --net0 virtio,bridge=vmbr0
-qm importdisk $virtualMachineId $imageName $volumeName
+qm importdisk $virtualMachineId ${imageName/.img/-resized.img} $volumeName
 qm set $virtualMachineId --scsihw virtio-scsi-pci --scsi0 $volumeName:vm-$virtualMachineId-disk-0
 qm set $virtualMachineId --boot c --bootdisk scsi0
 qm set $virtualMachineId --ide2 $volumeName:cloudinit
